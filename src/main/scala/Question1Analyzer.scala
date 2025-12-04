571472378744fb941026d5c23537052652ed23cd
xuxuannn <limhanxuan2002@gmail.com>
2025-12-05 01:55:40 +0800

    Update Question1Analyzer.scala

--- src/main/scala/Question1Analyzer.scala
+++ src/main/scala/Question1Analyzer.scala
@@ -9,21 +9,17 @@ object Question1Analyzer {
       println("❌ No data available")
       return
     }
-
-    // Use groupBy to group bookings by destination country
+    
     val bookingsByCountry = bookings.groupBy(_.destinationCountry)
-
-    // Use mapValues to count bookings per country
+    
     val countryCounts = bookingsByCountry.mapValues(_.size)
-
-    // Use maxBy to find the country with maximum bookings
+    
     val (topCountry, count) = countryCounts.maxBy(_._2)
 
     println(s"$topCountry has the highest number of bookings")
     println(s"Total bookings to $topCountry: $count")
     println(s"Percentage of all bookings: ${(count.toDouble / bookings.size * 100).formatted("%.1f")}%")
-
-    // Show top 5 countries using sortBy and take
+    
     println("\nTOP 5 COUNTRIES BY BOOKINGS:")
     val top5 = countryCounts.toList.sortBy(-_._2).take(5)
     top5.zipWithIndex.foreach { case ((country, count), index) =>
